05690fee8f2c842fcb603f3ebf43ba0c3ee34927
izambl <izambl@localhost.localdomain>
2017-10-17 15:56:35 -0500

    Print priorities object

--- src/app/components/sections/tdap-my-goals/tdap-my-goals.js
+++ src/app/components/sections/tdap-my-goals/tdap-my-goals.js
@@ -4,13 +4,15 @@ import ApiService from 'app/services/tdap-api-services';
 @inject(ApiService)
 export class TdapMyGoals
 {
+    priorities = [];
+
     constructor(api) {
         this.api = api;
     }
 
     activate() {
         this.api.getPriorities().then(p => {
-            console.log(p);
+            this.priorities = p;
         });
     }
 }
